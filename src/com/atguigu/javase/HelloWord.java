731e170d84b38d6e6ac03cc7b8c2239cdac8494c
xuxin <xuxin@126.com>
2017-07-26 21:06:00 +0800

    123123

---
+++ src/com/atguigu/javase/HelloWord.java
@@ -0,0 +1,24 @@
+package com.atguigu.javase;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * Hello Word
+ * 
+ * @author Xx
+ *
+ */
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// 插入排序
+		int[] a = { 10, 32, 12, 6, 51, 84, 9, 45, 65 };
+		Arrays.sort(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
